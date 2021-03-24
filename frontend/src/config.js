b593ef1bc15b1c58e9180485645ec6b66f23c016
Aurel Landry KENGNI NKONGTCHOU <lkengni@tagerim.fr>
2021-03-24 17:54:24 +0100

    Fix sur protocole WS selon environnement

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,7 +1,8 @@
 let config = {
     host: (process.env.NODE_ENV === 'production') ? `https://${window.location.hostname}/api` : 'http://localhost:3001',
     // Activer les logs côté navigateur selon l'environnement
-    debug: (process.env.NODE_ENV === 'production') ? false : true
+    debug: (process.env.NODE_ENV === 'production') ? false : true,
+    wsProtocol:  (process.env.NODE_ENV === 'production') ? 'wss' : 'ws',
 }
 
 export default config;
